d3f83d1a964c5445b6f9ede011622a3d8a2d6191
John Doe <79988159+John8790909@users.noreply.github.com>
2023-02-19 11:38:09 -0500

    added modular python code

--- python/main.py
+++ python/main.py
@@ -1,29 +1,19 @@
 import matplotlib.pyplot as plt
+from lib import get_x_coords, get_y_coords, draw_vec
 
-
-x = []
-y = []
-
-with open('../data/max_steps.txt', 'r') as f:
-    for line in f:
-        x.append(int(line))
-        
-with open('../data/max_step_gap.txt', 'r') as f:
-    for line in f:
-        y.append(int(line))
+x = get_x_coords()
+y = get_y_coords()
         
 
 xmin, xmax = xlim = 0, max(x) + max(x) * 0.1
 ymin, ymax = ylim = 0, max(y) + max(y) * 0.1
 
 fig, ax = plt.subplots()
+
 ax.set(xlim=xlim, ylim=ylim, autoscale_on=False)
-
-    
-
-# plot the data into a bar chart
-
 ax.bar(x, y, width=0.01, align='center', color='blue', edgecolor='black')
-
 ax.set_aspect('auto')
+
+draw_vec()
+
 plt.show()
--- src/utils/save_data.rs
+++ src/utils/save_data.rs
@@ -1,11 +1,26 @@
 use std::fs;
 use std::io::{Write, Result};
+use std::time::{SystemTime, UNIX_EPOCH};
 
+fn save_to_log(log: String) -> Result<()> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap();
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create_new(true)
+        .open(format!("./data/log/{:?}.txt", now)).unwrap();
+
+    file.set_len(0).unwrap();
+
+    file.write_all(log.as_bytes()).unwrap();
+
+    Ok(())
+}
 
 pub fn save_data(max_steps: Vec<i32>, max_step_gap: Vec<i32>) -> Result<()> {
     println!("saving data...");
-
-    
     // write max_steps
 
     let mut file = fs::OpenOptions::new()
@@ -16,8 +31,8 @@ pub fn save_data(max_steps: Vec<i32>, max_step_gap: Vec<i32>) -> Result<()> {
     
     file.set_len(0).unwrap();
 
-    for step in max_steps {
-        file.write_all(format!("{}\n", step).as_bytes()).unwrap();
+    for step in &max_steps {
+        file.write_all(format!("{}\n", *step).as_bytes()).unwrap();
     }
 
     // write max_step_gap to file
@@ -30,9 +45,18 @@ pub fn save_data(max_steps: Vec<i32>, max_step_gap: Vec<i32>) -> Result<()> {
 
     file.set_len(0).unwrap();
 
-    for step in max_step_gap {
-        file.write_all(format!("{}\n", step).as_bytes()).unwrap();
+    for step in &max_step_gap {
+        file.write_all(format!("{}\n", *step).as_bytes()).unwrap();
     }
+
+    // write log
+
+    let formated_max_steps = max_steps.into_iter().map(|x| -> String { format!("{}, ", x) }).collect::<String>();
+    let formated_max_step_gap = max_step_gap.into_iter().map(|x| -> String { format!("{}, ", x) }).collect::<String>();
+
+    let log = format!("/// MAX STEPS ///:\n {:?}, \n///MAX STEP GAP ///:\n {:?}", formated_max_steps, formated_max_step_gap);
+
+    save_to_log(log).expect("failed to save log :(");
 
     Ok(())
 }
